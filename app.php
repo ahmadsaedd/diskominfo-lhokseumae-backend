6f5e9abf376eae30783f2b60431ec5b35c9874f4
Muhammad Jundullah <sayidmuhammad15@gmail.com>
2025-01-02 22:31:41 +0700

    update

--- app.php
+++ app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\Cors;
-use \Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        \App\Http\Middleware\CorsMiddleware::class,
-    ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $frontendUrl = env('ORIGIN'); // add this line  
+        $response->headers->set('Access-Control-Allow-Credentials', 'true'); //set this if you are using axios
+        $response->headers->set('Access-Control-Allow-Origin', $frontendUrl);
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+
+        return $response;
+    }
+}
